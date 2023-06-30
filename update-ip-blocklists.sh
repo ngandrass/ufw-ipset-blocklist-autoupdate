477b080913d7d71fa1c2e73c6af6bdb8fa397670
Niels Gandraß <niels@gandrass.de>
2023-06-30 17:57:26 +0200

    Support for IPv6 blocklist parsing and ipset creation

--- update-ip-blocklists.sh
+++ update-ip-blocklists.sh
@@ -37,10 +37,13 @@ IPSET_BIN="/usr/bin/ipset"  # Path to ipset binary. Updated by detect_ipset().
 IPSET_DIR="/var/lib/ipset"  # Folder to write ipset save files to
 IPSET_PREFIX="blocklist"    # Prefix for ipset names
 IPSET_TYPE="hash:net"       # Type of created ipsets
+IPV4=1                      # Enable IPv4 by default
+IPV6=1                      # Enable IPv6 by default
 QUIET=0                     # Default quiet mode setting
 VERBOSE=0                   # Default verbosity level
 declare -A BLOCKLISTS       # Array for blocklists to use. Populated by CLI args,
 IPV4_REGEX="(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/[1-3]?[0-9])?" # Regex for a valid IPv4 address with optional subnet part
+IPV6_REGEX="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(/[1-6]?[0-9])?" # Regef for a valid IPv6 address with optional subnet part
 
 ##
 # Prints the help/usage message
@@ -53,6 +56,8 @@ Blocking lists of IPs from public blocklists / blacklists (e.g. blocklist.de, sp
 Options:
   -l     : Blocklist to use. Can be specified multiple times.
            Format: "\$name \$url" (space-separated). See examples below.
+  -4     : Run in IPv4 only mode. Ignore IPv6 addresses.
+  -6     : Run in IPv6 only mode. Ignore IPv4 addresses.
   -q     : Quiet mode. Outputs are suppressed if flag is present.
   -v     : Verbose mode. Prints additional information during execution.
   -h     : Print this help message.
@@ -60,6 +65,7 @@ Options:
 Example usage:
 $0 -l "spamhaus https://www.spamhaus.org/drop/drop.txt"
 $0 -l "blocklist https://lists.blocklist.de/lists/all.txt" -l "spamhaus https://www.spamhaus.org/drop/drop.txt"
+$0 -l "spamhaus https://www.spamhaus.org/drop/drop.txt" -l "spamhaus6 https://www.spamhaus.org/drop/dropv6.txt"
 EOF
 }
 
@@ -143,7 +149,44 @@ function validate_blocklists() {
 }
 
 ##
-# Updates the given blocklist
+# Updates an ipset based on a list of IP addresses 
+#
+# Arguments:
+#   $1 Name of the ipset to update
+#   $2 File containing all IP addresses to store in ipset
+#   $3 Procotol family (e.g. inet OR inet6)
+function update_ipset() {
+    # Setup local vars
+    local setname=$1
+    local ipfile=$2
+    local family=$3
+
+    # Create temporary ipset to build and ensure existence of live ipset
+    local livelist="$setname-$family"
+    local templist="$setname-$family-tmp"
+
+    $IPSET_BIN create -q "$livelist" "$IPSET_TYPE" family $family
+    $IPSET_BIN create -q "$templist" "$IPSET_TYPE" family $family
+    log_verbose "Prepared ipset lists: livelist='$livelist', templist='$templist'"
+
+    while read -r ip; do
+        $IPSET_BIN add "$templist" "$ip" || exit
+        log_verbose "Added '$ip' to '$templist'"
+    done < "$ipfile"
+
+    $IPSET_BIN swap "$templist" "$livelist"
+    log_verbose "Swapped ipset: $livelist"
+    $IPSET_BIN destroy "$templist"
+    log_verbose "Destroyed ipset: $templist"
+
+    # Write ipset savefile
+    $IPSET_BIN save "$livelist" > "$IPSET_DIR/$livelist.save"
+    log_verbose "Wrote savefile for '$livelist' to: $IPSET_DIR/$livelist.save"
+    log "Added $(cat "$ipfile" | wc -l) to ipset '$livelist'"
+}
+
+##
+# Updates the given blocklist from an URL
 #
 # Arguments:
 #   $1 Name of the blocklist
@@ -158,37 +201,34 @@ function update_blocklist() {
 
     # Check downloaded list
     linecount=$(cat "$tempfile" | wc -l)
-    if [ $linecount -lt 100 ]; then
+    if [ $linecount -lt 10 ]; then
         log_error "Blacklist '$1' containes only $linecount lines. This seems to short. Exiting..."
         exit 1
     fi
 
     # Extract ips from raw list data
-    grep -v '^[#;]' "$tempfile" | grep -E -o "$IPV4_REGEX" | cut -d ' ' -f 1 > "$tempfile.filtered"
-    log_verbose "Got $(cat "$tempfile.filtered" | wc -l) entries from blocklist '$1'"
+    if [[ $IPV4 -eq 1 ]]; then
+        grep -v '^[#;]' "$tempfile" | grep -E -o "$IPV4_REGEX" | cut -d ' ' -f 1 > "$tempfile.filtered"
+        local numips=$(cat "$tempfile.filtered" | wc -l)
+        log_verbose "Got $numips IPv4 entries from blocklist '$1'"
 
-    # Update ipset for blocklist
-    local livelist="${IPSET_PREFIX}-$1"
-    local templist="${IPSET_PREFIX}-$1-tmp"
+        if [[ $numips -gt 0 ]]; then
+            update_ipset "${IPSET_PREFIX}-$1" "$tempfile.filtered" "inet"
+        else
+            log_verbose "No IPv4 addresses found in blocklist '$1'. Skipping"
+        fi
+    fi
+    if [[ $IPV6 -eq 1 ]]; then
+        grep -v '^[#;]' "$tempfile" | grep -E -o "$IPV6_REGEX" | cut -d ' ' -f 1 > "$tempfile.filtered6"
+        local numips=$(cat "$tempfile.filtered6" | wc -l)
+        log_verbose "Got $numips IPv6 entries from blocklist '$1'"
 
-    $IPSET_BIN create -q "$livelist" "$IPSET_TYPE"
-    $IPSET_BIN create -q "$templist" "$IPSET_TYPE"
-    log_verbose "Prepared ipset lists: livelist='$livelist', templist='$templist'"
-
-    while read -r ip; do
-        $IPSET_BIN add "$templist" "$ip" || exit
-        log_verbose "Added '$ip' to '$templist'"
-    done < "$tempfile.filtered"
-
-    $IPSET_BIN swap "$templist" "$livelist"
-    log_verbose "Swapped ipset: $livelist"
-    $IPSET_BIN destroy "$templist"
-    log_verbose "Destroyed ipset: $templist"
-
-    # Write ipset savefile
-    $IPSET_BIN save "$livelist" > "$IPSET_DIR/$livelist.save"
-    log_verbose "Wrote savefile for '$livelist' to: $IPSET_DIR/$livelist.save"
-    log "Added $(cat "$tempfile.filtered" | wc -l) from blocklist '$1' to ipset '$livelist'"
+        if [[ $numips -gt 0 ]]; then
+            update_ipset "${IPSET_PREFIX}-$1" "$tempfile.filtered6" "inet6"
+        else
+            log_verbose "No IPv6 addresses found in blocklist '$1'. Skipping"
+        fi
+    fi
 
     # Cleanup
     rm "$tempfile"*
@@ -215,9 +255,17 @@ function main() {
 }
 
 # Parse arguments
-while getopts ":hvl:" opt; do
+while getopts ":hqv46l:" opt; do
     case ${opt} in
         l) BLOCKLISTS[${#BLOCKLISTS[@]}]=${OPTARG}
+            ;;
+        4) IPV4=1
+           IPV6=0
+           log "Using IPv4 only mode. Skipping IPv6 addresses."
+            ;;
+        6) IPV4=0
+           IPV6=1
+           log "Using IPv6 only mode. Skipping IPv4 addresses."
             ;;
         q) QUIET=1
             ;;
